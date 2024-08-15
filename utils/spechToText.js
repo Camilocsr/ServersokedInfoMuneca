2aa7c073924a8efd45b2b38900cf5b82e5f45390
Camilocsr <esmunred@gmail.com>
2024-08-15 14:28:03 -0500

    Se hace el cambbio de AsembblyIA a wisper de OpenIA

--- utils/spechToText.js
+++ utils/spechToText.js
@@ -1,6 +1,12 @@
 import 'dotenv/config';
-import { AssemblyAI } from 'assemblyai';
-import { deleteFile } from './deleteFile.js';
+import fs from "fs";
+import OpenAI from "openai";
+
+// en dado caso que desees usar Assembly el cual es mas preciso.
+//import { AssemblyAI } from 'assemblyai';
+//import { deleteFile } from './deleteFile.js';
+
+const openai = new OpenAI();
 
 /**
  * The function transcribeAudio asynchronously transcribes audio from a file using AssemblyAI's Speech
@@ -12,26 +18,42 @@ import { deleteFile } from './deleteFile.js';
  * @returns The function `transcribeAudio` is returning the transcribed text from the audio file after
  * processing it through AssemblyAI's transcription service.
  */
-export const transcribeAudio = async (audioFilePath) => {
-  console.log(audioFilePath)
-  const client = new AssemblyAI({
-    apiKey: process.env.ApiKeyAssembly
-  });
 
+// export const transcribeAudio = async (audioFilePath) => {
+//   console.log(audioFilePath)
+//   const client = new AssemblyAI({
+//     apiKey: process.env.ApiKeyAssembly
+//   });
+
+//   try {
+//     const params = {
+//       audio: audioFilePath,
+//       language_code: "es"
+//     };
+
+//     const transcript = await client.transcripts.transcribe(params);
+
+//     await deleteFile(audioFilePath);
+
+//     console.log(`esto es lo que se trascribe de el audio: ${transcript.text}`)
+//     return transcript.text;
+//   } catch (error) {
+//     console.error(`Ocurrió un error en el proceso de Speech to text de AssemblyAI: ${error}`);
+//     throw error;
+//   }
+// };
+
+export const transcribeAudio =  async (audioFilePath)=> {
   try {
-    const params = {
-      audio: audioFilePath,
-      language_code: "es"
-    };
+    const response = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(audioFilePath),
+      model: "whisper-1"
+    });
 
-    const transcript = await client.transcripts.transcribe(params);
-
-    await deleteFile(audioFilePath);
-
-    console.log(`esto es lo que se trascribe de el audio: ${transcript.text}`)
-    return transcript.text;
+    console.log(`Transcripción: ${response.text}`);
+    return response.text;
   } catch (error) {
-    console.error(`Ocurrió un error en el proceso de Speech to text de AssemblyAI: ${error}`);
+    console.error(`Error en la transcripción de audio: ${error.message}`);
     throw error;
   }
-};
+}
